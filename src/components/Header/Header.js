68c2590e5233f682b6fef0085fa3d35aa4e8bfeb
Esdras Chaves <esdraslchaves@gmail.com>
2020-05-11 15:24:21 -0400

    Set up Jest for unit testing

---
+++ src/components/Header/Header.js
@@ -0,0 +1,5 @@
+import React from "React"
+
+const Header = ({ title }) => <header>{title}</header>
+
+export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
 
-const IndexPage = () => <h1>Mavennet</h1>
+import Header from "../components/Header/Header"
+
+const IndexPage = () => (
+  <>
+    <Header title="This is a nice header" />
+    <h1>Mavennet</h1>
+  </>
+)
 
 export default IndexPage
