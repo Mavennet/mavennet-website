70b8d583a4302f4f39eb9f3b0808ec90ee8d70f4
Esdras Chaves <esdraslchaves@gmail.com>
2020-05-11 15:42:16 -0400

    Removes pwa option for now

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,20 +16,17 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-netlify-cms`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `mavennet-website`,
-        short_name: `mavennet`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `mavennet-website`,
+    //     short_name: `mavennet`,
+    //     start_url: `/`,
+    //     background_color: `#663399`,
+    //     theme_color: `#663399`,
+    //     display: `minimal-ui`,
+    //     icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+    //   },
+    // },
   ],
 }
