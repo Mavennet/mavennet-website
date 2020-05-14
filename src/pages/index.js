6907deec569f7e3a5b544df12789d7ca5d402082
Esdras Chaves <esdraslchaves@gmail.com>
2020-05-14 11:19:23 -0400

    Adds cms for Home Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,58 @@ import React from "react"
 
 import Header from "../components/Header/Header"
 
-const IndexPage = () => (
-  <>
-    <Header title="This is a nice header" />
-    <h1>Mavennet</h1>
-  </>
-)
+const IndexPage = ({ data }) => {
+  const post = data.pagesYaml
+
+  return <pre>{JSON.stringify(post, null, 2)}</pre>
+}
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    pagesYaml(identifier: { eq: "home" }) {
+      title
+      homeMainSection {
+        ctaText
+        slogan
+      }
+      homeServiceSection {
+        steps {
+          item {
+            image
+            title
+            description
+          }
+        }
+      }
+      homeSolutionsSection {
+        item {
+          title
+          image
+          description
+          slug
+          ctaText
+        }
+      }
+      homePartnersSection {
+        partners {
+          item {
+            title
+            image
+            link
+          }
+        }
+      }
+      homeNewsSection {
+        news {
+          item {
+            title
+            image
+            link
+          }
+        }
+      }
+    }
+  }
+`
