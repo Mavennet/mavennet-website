3af6477aa18074eb4c61a5a2d3424d83ac6bac1a
Esdras Chaves <esdraslchaves@gmail.com>
2020-05-21 10:34:50 -0400

    Adds frame in the news card

--- src/components/shared/NewsCard/styles.js
+++ src/components/shared/NewsCard/styles.js
@@ -3,14 +3,17 @@ import styled from "styled-components"
 export const NewsCard = styled.article`
   width: 100%;
   max-width: 385px;
-  padding-bottom: 0.3125em;
+  padding-bottom: 2em;
+
+  border: 1px solid var(--c-p-medium-purple);
 `
 
 export const CardLink = styled.a`
-  widht: 100%;
+  width: 100%;
 `
 
 export const Image = styled.img`
+  object-fit: cover;
   display: block;
   width: 100%;
 `
@@ -18,5 +21,7 @@ export const Image = styled.img`
 export const Title = styled.h4`
   color: var(--c-p-haiti);
 
-  margin-top: 0.83em;
+  margin: 0.83em 0.875em 0;
+
+  min-height: 87px;
 `
