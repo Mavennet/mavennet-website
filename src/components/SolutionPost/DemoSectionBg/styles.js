f41a0065aab09d78d2fd1b8e1d968a6a68339a5a
Esdras Chaves <esdraslchaves@gmail.com>
2020-05-28 14:23:13 -0400

    fixes text alignment demosectionbg

--- src/components/SolutionPost/DemoSectionBg/styles.js
+++ src/components/SolutionPost/DemoSectionBg/styles.js
@@ -29,12 +29,12 @@ export const Title = styled.h3`
   font-size: 2.125rem;
   font-weight: 600;
   line-height: 41px;
+  text-align: center;
 
   @media ${device.laptop} {
     font-size: 2.5em;
     font-weight: bold;
     line-height: 48px;
-    text-align: center;
   }
 `
 
