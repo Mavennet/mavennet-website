59c8f4ef1392395235f928559e1caf6c761c7e87
Esdras Chaves <esdraslchaves@gmail.com>
2020-05-27 23:44:32 -0400

    Request demo responsive

--- src/components/SolutionPost/DemoSection/styles.js
+++ src/components/SolutionPost/DemoSection/styles.js
@@ -2,25 +2,48 @@ import styled from "styled-components"
 
 import Button from "../../shared/Button"
 
-export const DemoSection = styled.section``
+import { device } from "../../../consts/deviceSizes"
+
+export const DemoSection = styled.section`
+  @media ${device.laptop} {
+    display: flex;
+  }
+`
 
 export const Image = styled.img`
   width: 100%;
   display: block;
+
+  @media ${device.laptop} {
+    width: 60%;
+
+    object-fit: cover;
+  }
 `
 
 export const TextContent = styled.div`
   background-color: var(--c-p-selago-dark);
 
   padding: 4.625em 2em;
+
+  @media ${device.desktop} {
+    padding: 9.625em 17.72em 9.625em 8.75em;
+  }
 `
 
 export const Title = styled.h3`
   font-size: 2.125rem;
   font-weight: 600;
   line-height: 41px;
+
+  @media ${device.desktop} {
+    font-size: 2.5rem;
+  }
 `
 
 export const DemoButton = styled(Button)`
   margin-top: 2.625em;
+  @media ${device.desktop} {
+    margin-top: 5.625em;
+  }
 `
