4a948bb6863b7a1e91e7f3a6180b56694cbaa8f6
Esdras Chaves <esdraslchaves@gmail.com>
2020-06-22 13:43:35 -0400

    Basic timeline data setup

---
+++ src/content/timeline.js
@@ -0,0 +1,82 @@
+export const RELEVANCE = {
+  LOW: "low",
+  MEDIUM: "medium",
+  HIGH: "high",
+}
+
+export const BUBBLE_STYLE = {
+  WHITE: "white",
+  LIGHT_PINK: "light-pink",
+  DARK_PINK: "dark-pink",
+}
+
+export const TimelineContent = {
+  "2018": {
+    yearImage: "2018-image.png",
+    events: [
+      {
+        title: "Mavennet is founded.",
+        subtitle:
+          "We are born from a new structure created to maximize growth and adoption of technology.",
+        logo: "mavennet-old-logo.svg",
+        glyph: "mavennet-old-logo.svg",
+        alt: "Mavennet Old Logo",
+        relevance: RELEVANCE.HIGH,
+        bubbleStyle: BUBBLE_STYLE.LIGHT_PINK,
+      },
+      {
+        title: "Mavennet partners with the TMX ",
+        logo: "tmx-logo.svg",
+        glyph: "tmx-logo.svg",
+        alt: "Tmx Logo",
+        relevance: RELEVANCE.HIGH,
+        bubbleStyle: BUBBLE_STYLE.DARK_PINK,
+      },
+      {
+        title: "The ‘token bridge’ is launched.",
+        subtitle:
+          "The interoperability application successfully transfers AION ERC-20 tokens from Ethereum to native AION coins on the Aion blockchain.",
+        logo: "bridge-logo.svg",
+        glyph: "bridge-logo.svg",
+        alt: "Token Bridge Logo",
+        relevance: RELEVANCE.LOW,
+        bubbleStyle: BUBBLE_STYLE.WHITE,
+      },
+      {
+        title: "Mavennet’s Hire #1",
+        logo: "mavennet-logo.svg",
+        glyph: "mavennet-logo.svg",
+        alt: "Mavennet Logo",
+        relevance: RELEVANCE.MEDIUM,
+        bubbleStyle: BUBBLE_STYLE.LIGHT_PINK,
+      },
+      {
+        title:
+          "Mavennet represents Canada as part of a tech delegation to the EU",
+        logo: "eu-logo.svg",
+        glyph: "eu-logo.svg",
+        alt: "EU Logo",
+        relevance: RELEVANCE.MEDIUM,
+        bubbleStyle: BUBBLE_STYLE.WHITE,
+      },
+      {
+        title: "Mavennet became a MaRs company",
+        logo: "mars-logo.svg",
+        glyph: "mars-logo.svg",
+        alt: "MaRS Logo",
+        relevance: RELEVANCE.LOW,
+        bubbleStyle: BUBBLE_STYLE.DARK_PINK,
+      },
+      {
+        title: "Mavennet became a IRAP company",
+        logo: "irap-logo.svg",
+        glyph: "irap-logo.svg",
+        alt: "IRAP Logo",
+        relevance: RELEVANCE.HIGH,
+        bubbleStyle: BUBBLE_STYLE.WHITE,
+      },
+    ],
+  },
+  "2019": {},
+  "2020": {},
+}
