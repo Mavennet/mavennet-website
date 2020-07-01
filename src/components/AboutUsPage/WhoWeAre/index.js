e49fd87492e9ba1cf70abedb9c4be8b5ff447081
Esdras Chaves <esdraslchaves@gmail.com>
2020-06-30 20:23:30 -0400

    Who we are section missing video

--- src/components/AboutUsPage/WhoWeAre/index.js
+++ src/components/AboutUsPage/WhoWeAre/index.js
@@ -1,18 +1,21 @@
 import React from "react"
 
-import Container from "../../base/Container"
-
 import * as S from "./styles"
 
 const WhoWeAre = ({ title, subTitle, description, ctaText }) => {
   return (
     <S.WhoWeAre>
-      <Container center>
-        <S.Title>{title}</S.Title>
-        <S.SubTitle>{subTitle}</S.SubTitle>
-        <S.Description>{description}</S.Description>
-        <S.CTA text={ctaText}></S.CTA>
-      </Container>
+      <S.Title>{title}</S.Title>
+      <S.SectionContainer center>
+        <S.TextContent>
+          <S.SubTitle>{subTitle}</S.SubTitle>
+          <S.Description>{description}</S.Description>
+          <S.CTA text={ctaText}></S.CTA>
+        </S.TextContent>
+        <S.VideoWrapper>
+          <S.IFrame src="https://www.youtube.com/embed/dQw4w9WgXcQ"></S.IFrame>
+        </S.VideoWrapper>
+      </S.SectionContainer>
     </S.WhoWeAre>
   )
 }
--- src/components/AboutUsPage/WhoWeAre/styles.js
+++ src/components/AboutUsPage/WhoWeAre/styles.js
@@ -1,60 +1,114 @@
 import styled from "styled-components"
 
+import Container from "../../base/Container"
 import TextArrowCTA from "../../shared/TextArrowCTA"
 
 import { device } from "../../../consts/deviceSizes"
 
 export const WhoWeAre = styled.section`
-  padding-bottom: 2.375em;
+  padding: 2.375em 0;
 
   @media ${device.tablet} {
     padding: 4.375em 0;
   }
 `
 
-export const Title = styled.h2`
-  font-size: 2.5rem;
-  font-weight: bold;
-  line-height: 48px;
+export const SectionContainer = styled(Container)`
+  @media ${device.laptop} {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    margin-top: 48px;
+  }
+`
+
+export const TextContent = styled.div`
+  box-sizing: border-box;
+  @media ${device.laptop} {
+    width: 50%;
+    padding-right: 3em;
+  }
+`
+
+export const VideoWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 219px;
+  width: 347px;
+
+  margin-top: 54px;
 
   @media ${device.laptop} {
-    font-size: 3rem;
-    line-height: 58px;
-    text-align: center;
+    width: 50%;
+    height: auto;
+    margin-top: 0;
+  }
+`
+
+export const IFrame = styled.iframe`
+  @media ${device.laptop} {
+    height: 269px;
+    width: 424px;
+  }
+`
+
+export const Title = styled.h2`
+  font-size: 40px;
+  font-weight: bold;
+  letter-spacing: 0.25px;
+  line-height: 56px;
+  text-align: center;
+
+  @media ${device.laptop} {
+    font-size: 48px;
+    letter-spacing: 0;
+    line-height: 64px;
   }
 `
 
 export const SubTitle = styled.h3`
-  font-size: 2.125rem;
-  font-weight: 500;
-  line-height: 41px;
-  text-align: center;
+  font-size: 24px;
+  font-weight: 600;
+  letter-spacing: 0;
+  line-height: 34px;
 
-  margin-top: 0.6em;
+  text-align: left;
+
+  margin-top: 14px;
 
   @media ${device.laptop} {
-    margin-top: 1.4em;
+    font-size: 34px;
     font-weight: 600;
+    letter-spacing: 0.25px;
+    line-height: 41px;
   }
 `
 
 export const Description = styled.p`
-  font-size: 1.5rem;
-  font-weight: 400;
-  line-height: 29px;
+  font-size: 16px;
+  letter-spacing: 0.25px;
+  line-height: 24px;
 
-  margin-top: 2em;
+  margin-top: 14px;
 
   @media ${device.laptop} {
-    font-size: 1rem;
-    line-height: 41px;
-    margin-top: 1.875em;
-    text-align: center;
+    font-size: 16px;
+    letter-spacing: 0.25px;
+    line-height: 24px;
+    text-align: left;
+
+    margin-top: 22px;
   }
 `
 
 export const CTA = styled(TextArrowCTA)`
-  font-size: 1.5rem !important;
+  font-size: 24px;
+  font-weight: 500;
+  letter-spacing: 0;
+  line-height: 29px;
 
-  margin-top: 1.875em;
+  margin-top: 14px;
 `
