88faadba03a2c32b989285b4c358967832fd03c0
Esdras Chaves <esdraslchaves@gmail.com>
2020-07-06 13:43:59 -0400

    StoryTimeline lint

--- src/components/shared/StoryTimeline/index.js
+++ src/components/shared/StoryTimeline/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, createRef, useEffect } from "react"
+import React, { useState, useRef, useEffect } from "react"
 
 import StoryTimelineTree from "../StoryTimelineTree"
 import TimelineList from "../TimelineList"
