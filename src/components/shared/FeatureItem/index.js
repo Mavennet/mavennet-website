66f44c4608fe9470d0ae121edda5312ec5351a2e
Esdras Chaves <esdraslchaves@gmail.com>
2020-07-15 14:28:26 -0400

    Fixes FeatureItem

--- src/components/shared/FeatureItem/index.js
+++ src/components/shared/FeatureItem/index.js
@@ -5,12 +5,20 @@ import * as S from "./styles"
 const FeatureItems = ({ title, description, image, isEven }) => {
   return (
     <S.FeatureItem isEven={isEven}>
-      <S.ImageContainer isEven={isEven} data-aos="fade-up">
+      <S.ImageContainer
+        isEven={isEven}
+        data-aos="fade-up"
+        data-aos-offset="250"
+      >
         <S.Image src={image} alt={`${title} image`} />
       </S.ImageContainer>
       <S.TextContent>
-        <S.Title data-aos="fade-up">{title}</S.Title>
-        <S.Description data-aos="fade-up">{description}</S.Description>
+        <S.Title data-aos="fade-up" data-aos-offset="250">
+          {title}
+        </S.Title>
+        <S.Description data-aos="fade-up" data-aos-offset="250">
+          {description}
+        </S.Description>
       </S.TextContent>
     </S.FeatureItem>
   )
