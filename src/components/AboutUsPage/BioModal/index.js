ce64ce11cb01e8544df030c882291a6379bdb114
Esdras Chaves <esdraslchaves@gmail.com>
2020-08-06 12:59:41 -0400

    fix: prevent scroll when BioModal is openned

--- src/components/AboutUsPage/BioModal/index.js
+++ src/components/AboutUsPage/BioModal/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 
 import SocialNetLink from "../../shared/SocialNetLink"
 import { Dialog } from "@material-ui/core"
@@ -7,6 +7,16 @@ import * as S from "./styles"
 
 const BioModal = ({ teamMember, open, handleClose }) => {
   const { name, title, bio, linkedin, image } = teamMember
+  useEffect(() => {
+    if (!open) return
+    document.getElementsByTagName("html")[0].style.overflow = "hidden";
+
+    return (() => {
+      document.getElementsByTagName("html")[0].style = "";
+    })
+  }, [open]);
+
+  // for enabling
   return (
     <Dialog open={open} onClose={handleClose} maxWidth={false}>
       {teamMember && (
