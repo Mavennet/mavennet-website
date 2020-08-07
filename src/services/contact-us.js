63c02f05ab70d69a46b3a1cd933175745903604e
Esdras Chaves <esdraslchaves@gmail.com>
2020-08-07 11:03:36 -0400

    fix: adds mailer url to node env

--- src/services/contact-us.js
+++ src/services/contact-us.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const callContactUs = async clientInfo => {
-  return await axios.post(process.env.GATSBY_MAILER_URL + "contact-us", {
+  return await axios.post(process.env.GATSBY_MAILER_URL || process.env.MAILER_URL + "contact-us", {
     clientInfo,
   })
 }
