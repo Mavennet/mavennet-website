c0f2d60c380ed7575a86bca93ec5d2f3fca15cd5
Esdras Chaves <esdraslchaves@gmail.com>
2020-08-07 11:52:44 -0400

    debug: adds console.log to check variables

--- src/services/contact-us.js
+++ src/services/contact-us.js
@@ -1,6 +1,8 @@
 import axios from "axios"
 
 export const callContactUs = async clientInfo => {
+  console.log('gatsby ', process.env.GATSBY_MAILER_URL)
+  console.log('node ', process.env.MAILER_URL)
   return await axios.post(process.env.GATSBY_MAILER_URL || process.env.MAILER_URL + "contact-us", {
     clientInfo,
   })
